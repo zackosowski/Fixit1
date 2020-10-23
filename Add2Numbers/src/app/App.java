7dd3d9ca902bc63a0318682532c41286f3ef59db
zackosowski <ZACHARYO@MYSTMA.ORG>
2020-10-23 17:51:12 +0000

    Created assignment

--- Add2Numbers/src/app/App.java
+++ Add2Numbers/src/app/App.java
@@ -1,14 +1,31 @@
 /*
-Add 2 Numbers Challenge
+Fixit 1
 
-Create a program that has the user input two numbers and then outputs their sum.
+This program outputs all even numbers between two numbers given by the user, Change it so that it instead prints all ODD numbers.
 */
-
 
 package app;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a number.");
+        int num1 = input.nextInt();
+        input.nextLine();
+
+        System.out.println("Please enter a larger number.");
+        int num2 = input.nextInt();
+        input.nextLine();
+
+        for (int i = num1; i < num2; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+
     }
 }
