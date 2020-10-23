c66c7954afb2e71056ac15884e125205b5378846
zackosowski <62389933+zackosowski@users.noreply.github.com>
2020-10-23 12:47:51 -0500

    Initial commit

---
+++ Add2Numbers/src/app/App.java
@@ -0,0 +1,14 @@
+/*
+Add 2 Numbers Challenge
+
+Create a program that has the user input two numbers and then outputs their sum.
+*/
+
+
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+    }
+}
